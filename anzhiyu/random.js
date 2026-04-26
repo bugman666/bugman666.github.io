f00d95393d550f9b8ed68d1cf0f28ebe4aca3e71
bugman666 <2643819278@qq.com>
2026-04-26 17:12:28 +0800

    Site updated: 2026-04-26 17:12:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/26/关于节点/","2026/04/25/不知道多少次测试/","2026/04/25/我的第一篇/"];function toRandomPost(){
+var posts=["2026/04/26/关于节点/","2026/04/25/不知道多少次测试/","2026/04/25/入站必看/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
